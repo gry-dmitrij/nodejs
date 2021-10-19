17f279df0e3bd2798cf500048c74c904d239866f
Dmitriy Gryaznykh <gry-dmitrij@yandex.ru>
2021-10-19 22:45:54 +0300

    Создает функцию форматированного вывода простых чисел

---
+++ numbers.js
@@ -0,0 +1,54 @@
+const colors = require('colors');
+
+let start = +process.argv[2];
+let end = +process.argv[3];
+if (!Number.isInteger(start) || !Number.isInteger(end)) {
+    console.error(new Error('Не верный ввод. Введите два целых числа.'));
+    return
+}
+
+if (start > end) {
+    [start, end] = [end, start];
+}
+
+let arr = simpleNumbers(start, end);
+let str = '';
+arr.forEach((item,idx) => {
+    if ((idx + 1) % 3 === 0) {
+        str += `${item}`.red;
+    } else if ((idx + 1) % 3 === 2) {
+        str += `${item}`.yellow;
+    } else {
+        str += `${item}`.green;
+    }
+    str += ' ';
+})
+if (str.length > 0) {
+    console.log(str);
+} else {
+    console.log('В этом диапазоне нет простых чисел');
+}
+
+
+/**
+ * Возвращает массив простых чисел
+ * @param start {number} - начало диапазона
+ * @param end {number} - конец диапазона
+ * @returns {*[]} - массив простых чисел
+ */
+function simpleNumbers(start, end) {
+    const arr = [];
+    for (let i = start; i <= end; i++) {
+        let flag = true;
+        for (let j = 2; j * j <= i && i > 2; j++) {
+            if (i % j === 0) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag) {
+            arr.push(i);
+        }
+    }
+    return arr;
+}
