5f7e40f3b4e65a8229d82f7f0fbc150be096c01e
gryda <gryda@hismith.ru>
2021-10-29 22:37:26 +0300

    Добавляет запись в отдельные файлы Исправляет ошибки валидации Добавляет комментарии

--- ls3/logs.js
+++ ls3/logs.js
@@ -2,31 +2,49 @@ const fs = require('fs');
 const readline = require('readline');
 
 const INP_PATH = './logs/access-short.log'
-const OUT_PATH = './logs/logs.log'
-const [,, ...args] = process.argv;
+const OUT_PATH = './logs/'
+const [, , ...args] = process.argv;
 
-if (validate(args)) return;
+// валидируем введенные данные
+if (!validate(args)) return;
 
+// открываем поток для чтения
 const readStream = fs.createReadStream(INP_PATH, {
     encoding: 'utf-8'
 });
-const writeStream = fs.createWriteStream(OUT_PATH, {
-    encoding: "utf-8",
-});
+
+// создаем потоки для записи
+const maps = new Map();
+args.forEach(item => {
+    const writeStream = fs.createWriteStream(
+        `${OUT_PATH + item}_requests.log`,
+        {encoding: 'utf-8'},
+    );
+    maps.set(item.trim(), writeStream);
+    writeStream.on('error', err => {
+        console.log(err);
+    })
+})
+
 const lineReader = readline.createInterface({
     input: readStream,
 })
 
+// читаем построчно
 lineReader.on('line', (input) => {
-    let contain = false;
+    let contain = '';
     for (let i = 0; i < args.length; i++) {
         if (input.includes(args[i])) {
-            contain = true;
+            contain = args[i];
             break;
         }
     }
     if (contain) {
-        writeStream.write(input + '\n');
+        // достаем нужный поток из maps
+        const writeStream = maps.get(contain);
+        if (writeStream) {
+            writeStream.write(input + '\n');
+        }
     }
 })
 
@@ -34,10 +52,11 @@ readStream.on('error', err => {
     console.log(err);
 });
 
-writeStream.on('error', err => {
-    console.log(err);
-})
-
+/**
+ * Валидация введенных данных
+ * @param args {Array} - массив ip-адресов
+ * @returns {boolean} - true, если валидация пройдена, иначе false
+ */
 function validate(args) {
     if (args.length === 0) {
         console.error(new Error('Введите ip-адреса'));
@@ -48,8 +67,8 @@ function validate(args) {
         const ip = args[i];
         if (!/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/.test(ip.trim())) {
             console.error(new Error(`Неправильно введен ip-адрес: ${ip}`));
+            return false;
         }
-        return false;
     }
     return true;
 }
